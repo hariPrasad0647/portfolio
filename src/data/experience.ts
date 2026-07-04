cadd7fb00d4d5d1245e5a7e7c6304fcd8c4bada6
Hari Prasad <157706933+hariPrasad0647@users.noreply.github.com>
2026-07-04 21:09:49 +0530

    Update experience dates for Software Developer role

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -2,12 +2,22 @@ import { type ExperienceShowcaseListItemProps } from "@/components/experience/ex
 
 export const EXPERIENCE: ExperienceShowcaseListItemProps[] = [
   {
+    title: "Node.js Developer",
+    organisation: {
+      name: "OZRIT: Enterprise Software Development & AI Solutions",
+      href: "https://ozrit.com",
+    },
+    date: "Apr 2026 - Present",
+    location: "Hyderabad",
+    description: "Node.js development",
+  },
+  {
     title: "Software Developer",
     organisation: {
       name: "HanviTec Solutions Private Ltd",
       href: "https://www.hanvitec.com/",
     },
-    date: "Oct 2025 - Present",
+    date: "Oct 2025 - Apr 2026",
     location: "Hyderabad",
     description: "Nextjs and Nodejs web development",
   },
